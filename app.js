80dd82da4607636ec448a3c54c243b7313d216bb
driss292 <drisskaci@outlook.fr>
2022-06-24 17:52:43 +0200

    callApi

--- app.js
+++ app.js
@@ -1,4 +1,43 @@
 const searchInput = document.querySelector(".recherche-poke input");
+let allPokemon = [];
+let tableauFin = [];
+
+const fecthPokemenComplet = (pokemon) => {
+    let objPokemenFull = {};
+    let url = pokemon.url;
+    let nameP = pokemon.name;
+
+    fetch(url)
+        .then((res) => res.json())
+        .then((pokeData) => {
+            // console.log(pokeData);
+            objPokemenFull.pic = pokeData.sprites.front_default;
+            objPokemenFull.type = pokeData.types[0].type.name;
+
+            fetch(`https://pokeapi.co/api/v2/pokemon-species/${nameP}`)
+                .then((res) => res.json())
+                .then((pokeData) => {
+                    // console.log(pokeData);
+                    objPokemenFull.name = pokeData.names[4].name;
+                    allPokemon.push(objPokemenFull);
+                    if (allPokemon.length === 151) {
+                        console.log(allPokemon);
+                    }
+                });
+        });
+};
+const fecthPokemenBase = () => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+        .then((res) => res.json())
+        .then((allPoke) => {
+            // console.log(allPoke);
+            allPoke.results.forEach((pokemon) => {
+                fecthPokemenComplet(pokemon);
+            });
+        });
+};
+fecthPokemenBase();
+
 // Animation input
 
 searchInput.addEventListener("input", (e) => {
